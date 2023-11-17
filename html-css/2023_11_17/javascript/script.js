abae1005c5f575b0b4714291224e2f07b265280c
Cartu <l.carturan@gmail.com>
2023-11-17 11:37:09 +0100

    init javascript

---
+++ html-css/2023_11_17/javascript/script.js
@@ -0,0 +1,27 @@
+let age = 28;
+
+console.log("IF - ELIF - ELSE");
+if (age >= 18) {
+    console.log("You are an adult");
+} else if (age <= 0) {
+    console.log("You are not born yet");
+} else {
+    console.log("You are a minor");
+}
+
+console.log("\nFOR");
+for (let i = 0; i < 5; ++i) {
+  console.log(i);
+}
+
+console.log("\nWHILE");
+while (age < 30) {
+  console.log(age);
+  age++;
+}
+
+console.log("\nDO WHILE");
+do {
+    console.log(age);
+    age++;
+} while (age < 30);
