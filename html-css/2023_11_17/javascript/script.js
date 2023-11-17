6eed412ac81a547f1b894e03a8dda505216a0676
Cartu <l.carturan@gmail.com>
2023-11-17 11:44:22 +0100

    prompt javascript

--- html-css/2023_11_17/javascript/script.js
+++ html-css/2023_11_17/javascript/script.js
@@ -1,27 +1,31 @@
-let age = 28;
+// let age = 28;
 
-console.log("IF - ELIF - ELSE");
-if (age >= 18) {
-    console.log("You are an adult");
-} else if (age <= 0) {
-    console.log("You are not born yet");
-} else {
-    console.log("You are a minor");
-}
+// console.log("IF - ELIF - ELSE");
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else if (age <= 0) {
+//     console.log("You are not born yet");
+// } else {
+//     console.log("You are a minor");
+// }
 
-console.log("\nFOR");
-for (let i = 0; i < 5; ++i) {
-  console.log(i);
-}
+// console.log("\nFOR");
+// for (let i = 0; i < 5; ++i) {
+//   console.log(i);
+// }
 
-console.log("\nWHILE");
-while (age < 30) {
-  console.log(age);
-  age++;
-}
+// console.log("\nWHILE");
+// while (age < 30) {
+//   console.log(age);
+//   age++;
+// }
 
-console.log("\nDO WHILE");
-do {
-    console.log(age);
-    age++;
-} while (age < 30);
+// console.log("\nDO WHILE");
+// do {
+//     console.log(age);
+//     age++;
+// } while (age < 30);
+
+var nome = prompt("Inserisci il tuo nome: ");
+
+document.write("<h1 class=\"font-black\">Ciao " + nome + "!</h1>");
